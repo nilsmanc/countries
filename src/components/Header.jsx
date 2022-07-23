e631bdb6dd721a1c6d783e60bd41ea421c5a1aa2
nilsmanc <etereaiberia@gmail.com>
2022-07-23 14:44:49 +0300

    forms

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,30 @@ import { IoMoon, IoMoonOutline } from 'react-icons/io5';
 import { Container } from './Container';
 
 const HeaderEl = styled.header`
-box-shadow: var(--shadow)
-background-color: var(--color-ui-base)`;
-const Wrapper = styled.div``;
+  box-shadow: var(--shadow);
+  background-color: var(--color-ui-base);
+`;
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem 0;
+`;
 
-const Title = styled.a.attrs({ href: '/' })``;
+const Title = styled.a.attrs({ href: '/' })`
+  color: var(--colors-text);
+  font-size: var(--fs-sm);
+  text-decoration: none;
+  font-weight: var(--fw-bold);
+`;
 
-const ModeSwithcer = styled.div``;
+const ModeSwithcer = styled.div`
+  color: var(--colors-text);
+  font-size: var(--fs-sm);
+  cursor: pointer;
+  // font-weight: var(--fw-bold);
+  text-transform: capitalize;
+`;
 
 export const Header = () => {
   const [theme, setTheme] = useState('light');
@@ -24,7 +41,8 @@ export const Header = () => {
         <Wrapper>
           <Title>Where is the world?</Title>
           <ModeSwithcer onClick={toggleTheme}>
-            <IoMoon /> Light Theme
+            {theme === 'light' ? <IoMoonOutline size='14px' /> : <IoMoon size='14px' />}
+            <span style={{ marginLeft: '0.75rem' }}>{theme} Theme</span>
           </ModeSwithcer>
         </Wrapper>
       </Container>
